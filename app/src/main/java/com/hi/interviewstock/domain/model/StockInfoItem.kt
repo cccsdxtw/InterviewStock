ca92936b79cf045dfeb18a0034075ee0ecd8bbf5
donghao <mikedong@ptc-nec.com.tw>
2025-01-19 05:44:03 +0800

    加入綜合版本的Item

--- app/src/main/java/com/hi/interviewstock/domain/model/StockInfoItem.kt
+++ app/src/main/java/com/hi/interviewstock/domain/model/StockInfoItem.kt
@@ -69,3 +69,62 @@ data class StockDayAvgAllInfoItem(
     @SerializedName("MonthlyAveragePrice")
     val MonthlyAveragePrice: String? = "沒有資料",
 ) {}
+
+
+data class ALLInfoItem(
+    // 股票代碼，預設值為 "沒有資料"
+    @SerializedName("Code")
+    val code: String? = "沒有資料",
+
+    // 股票名稱，預設值為 "沒有資料"
+    @SerializedName("Name")
+    val name: String? = "沒有資料",
+
+    // 本益比（PE Ratio），預設值為 "沒有資料"
+    @SerializedName("PEratio")
+    val peRatio: String? = "沒有資料",
+
+    // 股息殖利率，預設值為 "沒有資料"
+    @SerializedName("DividendYield")
+    val dividendYield: String? = "沒有資料",
+
+    // 市價比（PB Ratio），預設值為 "沒有資料"
+    @SerializedName("PBratio")
+    val pbRatio: String? = "沒有資料",
+
+    // 收盤價，預設值為 "沒有資料"
+    @SerializedName("ClosingPrice")
+    val closingPrice: String? = "沒有資料",
+
+    // 月平均價格，預設值為 "沒有資料"
+    @SerializedName("MonthlyAveragePrice")
+    val monthlyAveragePrice: String? = "沒有資料",
+
+    // 交易量，預設值為 "沒有資料"
+    @SerializedName("TradeVolume")
+    val tradeVolume: String? = "沒有資料",
+
+    // 交易金額，預設值為 "沒有資料"
+    @SerializedName("TradeValue")
+    val tradeValue: String? = "沒有資料",
+
+    // 開盤價，預設值為 "沒有資料"
+    @SerializedName("OpeningPrice")
+    val openingPrice: String? = "沒有資料",
+
+    // 最高價，預設值為 "沒有資料"
+    @SerializedName("HighestPrice")
+    val highestPrice: String? = "沒有資料",
+
+    // 最低價，預設值為 "沒有資料"
+    @SerializedName("LowestPrice")
+    val lowestPrice: String? = "沒有資料",
+
+    // 漲跌價差，預設值為 "沒有資料"
+    @SerializedName("Change")
+    val change: String? = "沒有資料",
+
+    // 交易次數，預設值為 "沒有資料"
+    @SerializedName("Transaction")
+    val transaction: String? = "沒有資料",
+)
